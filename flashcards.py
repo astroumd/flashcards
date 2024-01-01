a60132c81e3e7015620e7caad70b0691c442dc11
Derek Richardson <dcr@umd.edu>
2023-12-31 18:30:20 -0700

    fix header comment

--- flashcards.py
+++ flashcards.py
@@ -13,7 +13,7 @@ Requirements:
 
 Usage notes:
     1. Prepare a folder with image photos. JPG and PNG are supported formats.
-    2. Currently the photo names are assumed to be of the form Last,First.png.
+    2. Currently photo names are assumed to be of form Last,First.extension.
     3. Invoke the script either in the folder or by specifying the folder path
        as a command-line argument.
     4. Choose from 2 modes: flashcard (default) or 4-choice quiz (-q option).
